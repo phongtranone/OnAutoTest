52cd96dccef81e796d860bf3d3c6d5fee5a22d95
Phong Tran <phongtranone@gmail.com>
2025-04-08 22:03:08 +0700

    Turn off parallel running

--- src/test/java/runner/RegressionTestGroup4Runner.java
+++ src/test/java/runner/RegressionTestGroup4Runner.java
@@ -16,9 +16,9 @@ import org.testng.annotations.DataProvider;
         monochrome = true)
 
 public class RegressionTestGroup4Runner extends AbstractTestNGCucumberTests{
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+//    @Override
+//    @DataProvider(parallel = true)
+//    public Object[][] scenarios() {
+//        return super.scenarios();
+//    }
 }
